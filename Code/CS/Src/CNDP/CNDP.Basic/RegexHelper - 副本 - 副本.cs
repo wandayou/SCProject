ce3d9d8b53efda4de71647f3750da4b840c5d088
wandayou <wandayou@sobey.com>
2024-02-21 13:37:43 +0800

    main copy file

--- Code/CS/Src/CNDP/CNDP.Basic/RegexHelper - 副本 - 副本.cs
+++ Code/CS/Src/CNDP/CNDP.Basic/RegexHelper - 副本 - 副本.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 
 namespace CNDP.Basic
 {
-//test01 g
+//test01
     public static class RegexHelper
     {
         private const string _dateTimePattern = @"[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):[0-5]\d:[0-5]\d";
