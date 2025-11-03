f58c5156b17b72004f76fcbb6a32ab0e74208865
wandayou <wandayou@sobey.com>
2025-11-03 14:59:58 +0800

    完善

--- Code/CS/Src/CNDP/CNDP.Basic/RegexHelper.cs
+++ Code/CS/Src/CNDP/CNDP.Basic/RegexHelper.cs
@@ -5,21 +5,14 @@ using System.Text.RegularExpressions;
 
 namespace CNDP.Basic
 {
-//test01
+    //test01
     public static class RegexHelper
     {
         private const string _dateTimePattern = @"[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):[0-5]\d:[0-5]\d";
 
         public static string MatchDateTime(string value)
         {
-            if (null == value)
-                return null;
-
-            Match dateTimeMatch = Regex.Match(value, _dateTimePattern);
-            if (dateTimeMatch.Success)
-                return dateTimeMatch.Value;
-            else
-                return null;
+            return "O1";
         }
     }
 }
